94056530b42970d521ab9f12a1ec4e4062afdc3c
Enmanuel Torres <eenmanuel.jose.17.1@gmail.com>
2024-12-20 10:41:02 -0500

    Initial Commit

---
+++ MaraFetch/MaraFetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MaraFetchApp.swift
+//  MaraFetch
+//
+//  Created by ENMANUEL TORRES on 20/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MaraFetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
